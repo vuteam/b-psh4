c36b38277757407ff94403505749866a4150edc9
vuteam <rakzami39@gmail.com>
2018-01-26 19:58:15 +0100

    Update enigma.cpp

--- main/enigma.cpp
+++ main/enigma.cpp
@@ -9,6 +9,7 @@
 #include <lib/actions/action.h>
 #include <lib/driver/rc.h>
 #include <lib/base/ioprio.h>
+#include <lib/base/e2avahi.h>
 #include <lib/base/ebase.h>
 #include <lib/base/eenv.h>
 #include <lib/base/eerror.h>
@@ -113,6 +114,7 @@ class eMain: public eApplication, public Object
 public:
 	eMain()
 	{
+	        e2avahi_init(this); 
 		init.setRunlevel(eAutoInitNumbers::main);
 		/* TODO: put into init */
 		m_dvbdb = new eDVBDB();
@@ -126,6 +128,7 @@ public:
 	{
 		m_dvbdb->saveServicelist();
 		m_mgr->releaseCachedChannel();
+		e2avahi_close();
 	}
 };
 
